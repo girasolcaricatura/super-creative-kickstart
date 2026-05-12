1e283a2d640bf190d5dd45f0e386423569c59946
girasolcaricatura <girasolcaricatura@gmail.com>
2026-05-11 20:22:41 -0600

    fix: kickstart simplificado sin config

--- api/kickstart.js
+++ api/kickstart.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -19,18 +19,9 @@ module.exports = async function handler(req, res) {
       },
       body: JSON.stringify(req.body),
     });
-
     const data = await response.json();
     return res.status(response.status).json(data);
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
-};
-
-module.exports.config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
-  },
 };
